63b3fe9a9dbeb918c60cd99245b6ffcbb6cbb28e
JohnCorby <williamforgames6@gmail.com>
2018-07-07 15:35:05 -0700

    ill fix this in a bit

--- src/com/johncorby/coreapi/Main.java
+++ src/com/johncorby/coreapi/Main.java
@@ -1,4 +1,24 @@
 package com.johncorby.coreapi;
 
-public class Main {
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    public static Main main;
+    public static FileConfiguration CONFIG;
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
 }
