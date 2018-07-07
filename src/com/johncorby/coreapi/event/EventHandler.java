736e7cc6750851dce3c07d64af7bf16954d7f4c8
JohnCorby <williamforgames6@gmail.com>
2018-07-07 15:36:35 -0700

    ill fix this in a bit

---
+++ src/com/johncorby/coreapi/event/EventHandler.java
@@ -0,0 +1,20 @@
+package com.johncorby.coreapi.event;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+
+import static com.johncorby.virtualredstone.VirtualRedstone.virtualRedstone;
+
+public class EventHandler {
+
+    public EventHandler() {
+        // Register events
+        register(new Block());
+
+        //new Any();
+    }
+
+    private static void register(Listener event) {
+        Bukkit.getPluginManager().registerEvents(event, virtualRedstone);
+    }
+}
